91b8785e245407f45b37502afb7ad031c80a04be
Adam Sandstedt <Adam.sandstedt@gmail.com>
2021-03-11 14:24:13 -0700

    Added a source code page

---
+++ source-code.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html>
+<meta name="viewport" content="width=device-width, initial-scale=1">
+
+<?php
+require_once 'users/init.php';
+require_once $abs_us_root.$us_url_root.'users/includes/template/prep.php';
+?>
+
+<style>
+body {
+  font-family: "Lato", sans-serif;
+}
+
+#main {
+  transition: margin-left .5s;
+  padding-left: 8px;
+}
+</style>
+
+<head>
+  <title>Crumble - Source Code</title>
+</head>
+
+<body>
+<div id="main">
+  <h2>Source Code</h2>
+  <p>
+    This website is being developed as an open source project led by <a href="https://github.com/AdamSandstedt">Adam Sandstedt.</a>
+    If you're interested in participating, go to the <a href="https://github.com/AdamSandstedt/Crumble-Online">Crumble-Online GitHub</a> page and start hacking around.
+    Adam also leads development on the <a href="https://github.com/AdamSandstedt/Crumble">Crumble stand-alone Java app</a>.
+    <br><br>
+    The <a href="https://groups.google.com/g/crumble?pli=1">Crumble mailing list</a> is open to everyone, and is the official place for announcements, developer discussion, and player conversation.
+  </p>
+</div>
+</body>
+
+</html>
